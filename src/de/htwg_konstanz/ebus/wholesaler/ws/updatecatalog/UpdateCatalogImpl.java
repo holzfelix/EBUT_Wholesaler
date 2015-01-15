c9d6baafae96f52d9eab3d5c5653b6bed5a4dd59
Felix Hohlwegler (holzfelix) <info@felix-hohlwegler.de>
2015-01-15 21:00:05 +0100

    Navigation für den Supplier angepasst, start implementierung des WS

--- src/de/htwg_konstanz/ebus/wholesaler/ws/updatecatalog/UpdateCatalogImpl.java
+++ src/de/htwg_konstanz/ebus/wholesaler/ws/updatecatalog/UpdateCatalogImpl.java
@@ -5,6 +5,9 @@
  */
 package de.htwg_konstanz.ebus.wholesaler.ws.updatecatalog;
 
+import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOProduct;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.ProductBOA;
+import java.util.Collection;
 import javax.xml.ws.ServiceMode;
 import javax.xml.ws.WebServiceProvider;
 
@@ -16,6 +19,16 @@ import javax.xml.ws.WebServiceProvider;
 @WebServiceProvider(serviceName = "UpdateCatalogWebService", portName = "updateCatalogPort", targetNamespace = "http://localhost:8084/EBUT_Wholesaler/updatecatalog", wsdlLocation = "WEB-INF/wsdl/UpdateCatalogImpl/updateCatalogue.wsdl")
 public class UpdateCatalogImpl implements javax.xml.ws.Provider<javax.xml.transform.Source>, UpdateCatalog {
 
+    /**
+     * Collection of the Products.
+     */
+    private Collection<BOProduct> products;
+
+    /**
+     * Object Factory.
+     */
+    private final ObjectFactory factory = new ObjectFactory();
+
     public javax.xml.transform.Source invoke(javax.xml.transform.Source source) {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
@@ -23,7 +36,26 @@ public class UpdateCatalogImpl implements javax.xml.ws.Provider<javax.xml.transf
 
     @Override
     public UpdateResponse updateCatalog(UpdateRequest updateRequest) throws AuthenticationFault {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        /**
+         * Generates a response object.
+         */
+        UpdateResponse response = factory.createUpdateResponse();
+        /**
+         * List of the Products which were send to the Service.
+         */
+        ListOfProducts productList = updateRequest.getListOfProducts();
+
+        ListOfUnavailableProducts unavailableProducts = factory.createListOfUnavailableProducts();
+        ListOfUpdatedProducts updatedProducts = factory.createListOfUpdatedProducts();
+
+        products = ProductBOA.getInstance().findAll();
+
+        for (SupplierProduct productSupplier : productList.getSupplierProduct()) {
+
+        }
+
+        return response;
     }
 
 }
